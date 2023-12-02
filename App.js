4300f2e89fee2e514c3065f41e55b956aa0f891c
DipanshiTyagi <143926420+DipanshiTyagi@users.noreply.github.com>
2023-12-02 15:54:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Wheel from './Wheel/Wheel';
+// import WheelComponent from './WheelComponent/WheelComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Wheel />
+        {/* <WheelComponent /> */}
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
